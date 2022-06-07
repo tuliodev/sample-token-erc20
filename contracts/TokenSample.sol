72a2fe7270f3a97e510268e6f3548a79638b118e
tuliodev <resendetulio51@gmail.com>
2022-06-07 18:46:58 -0300

    feat: add SafeMath to improve the security

--- contracts/TokenSample.sol
+++ contracts/TokenSample.sol
@@ -4,16 +4,42 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
+library SafeMath {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        uint256 c = a - b;
+        return c;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
 interface IERC20 {
-    function totalSupply() external returns(uint256);
-    function balanceOf(address _owner) external returns(uint256);
-    function transfer(address _to, uint256 _value) external returns(bool);
-    function transferFrom(address _from, address _to, uint256 _value) external returns(bool);
-    function approve(address _spender, uint256 _value) external returns(bool);
-    function allowance(address _owner, address _spender) external returns(uint256);
+    function totalSupply() external returns (uint256);
+
+    function balanceOf(address _owner) external returns (uint256);
+
+    function transfer(address _to, uint256 _value) external returns (bool);
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool);
+
+    function approve(address _spender, uint256 _value) external returns (bool);
+
+    function allowance(address _owner, address _spender)
+        external
+        returns (uint256);
 }
 
 contract TokenSample is IERC20 {
+    using SafeMath for uint256;
     string public constant name = "Token sample coin";
     string public constant symbol = "TSC";
     uint8 public constant decimals = 18;
@@ -45,21 +71,29 @@ contract TokenSample is IERC20 {
         return balances[_owner];
     }
 
-    function transfer(address _to, uint256 _value) public override returns (bool) {
+    function transfer(address _to, uint256 _value)
+        public
+        override
+        returns (bool)
+    {
         require(
             _value <= balances[msg.sender],
             "You must have this tokens on you account"
         );
 
-        balances[msg.sender] = balances[msg.sender] - _value;
-        balances[_to] += _value;
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
 
         emit Transfer(msg.sender, _to, _value);
 
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public override returns (bool) {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public override returns (bool) {
         require(
             _value <= balances[_from],
             "Owner must have the tokens value on account"
@@ -70,25 +104,32 @@ contract TokenSample is IERC20 {
             "Must be allowed to make this transaction"
         );
 
-        balances[_from] = balances[_from] - _value;
-        balances[_to] = balances[_to] + _value;
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
 
-        allowed[_from][_to] = allowed[_from][_to] - _value;
+        allowed[_from][_to] = allowed[_from][_to].sub(_value);
 
         emit Transfer(_from, _to, _value);
 
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public override returns (bool) {
+    function approve(address _spender, uint256 _value)
+        public
+        override
+        returns (bool)
+    {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
-    
-    function allowance(address _owner, address _spender) public view override returns (uint256) {
+
+    function allowance(address _owner, address _spender)
+        public
+        view
+        override
+        returns (uint256)
+    {
         return allowed[_owner][_spender];
     }
-
-
 }
